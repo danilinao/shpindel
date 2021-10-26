5444fa47f06fa26988c91f404ef954c8e3fca728
Alexey Danilin <adm.minefun@yandex.ru>
2021-10-26 11:22:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,250 @@
+
+function getCurrentDateString() {
+    return (new Date()).toISOString() + ' ::';
+};
+__originalLog = console.log;
+console.log = function () {
+    var args = [].slice.call(arguments);
+    __originalLog.apply(console.log, [getCurrentDateString()].concat(args));
+};
+
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+const { Readable } = require('stream');
+
+
+
+
+//////////////////////////////////////////
+///////////////// VARIA //////////////////
+//////////////////////////////////////////
+
+function necessary_dirs() {
+    if (!fs.existsSync('./data/')){
+        fs.mkdirSync('./data/');
+    }
+}
+necessary_dirs()
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function convert_audio(input) {
+    try {
+        // stereo to mono channel
+        const data = new Int16Array(input)
+        const ndata = data.filter((el, idx) => idx % 2);
+        return Buffer.from(ndata);
+    } catch (e) {
+        console.log(e)
+        console.log('convert_audio: ' + e)
+        throw e;
+    }
+}
+//////////////////////////////////////////
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+
+//////////////////////////////////////////
+//////////////// CONFIG //////////////////
+//////////////////////////////////////////
+
+//const SETTINGS_FILE = 'settings.json';
+
+let DISCORD_TOK = process.env.DISCORD_TOK;
+let PREFIX = process.env.PREFIX;
+
+//function loadConfig() {
+//    if (fs.existsSync(SETTINGS_FILE)) {
+//        const CFG_DATA = JSON.parse( fs.readFileSync(SETTINGS_FILE, 'utf8') );
+//        DISCORD_TOK = CFG_DATA.DISCORD_TOK;
+//		PREFIX = CFG_DATA.PREFIX;
+//    }
+//    DISCORD_TOK = process.env.DISCORD_TOK || DISCORD_TOK;
+//	PREFIX = process.env.PREFIX || PREFIX;
+//
+//    if (!DISCORD_TOK)
+//        throw 'invalid or missing DISCORD_TOK'
+//    
+//}
+//loadConfig()
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+
+const Discord = require('discord.js')
+const DISCORD_MSG_LIMIT = 2000;
+const discordClient = new Discord.Client()
+if (process.env.DEBUG)
+    discordClient.on('debug', console.debug);
+discordClient.on('ready', () => {
+    console.log(`Logged in as ${discordClient.user.tag}!`)
+})
+discordClient.login(DISCORD_TOK)
+
+
+const _CMD_JOIN        = PREFIX + 'join';
+const _CMD_LEAVE       = PREFIX + 'leave';
+
+const guildMap = new Map();
+
+discordClient.on('message', async (msg) => {
+    try {
+        if (!('guild' in msg) || !msg.guild) return; // prevent private messages to bot
+        const mapKey = msg.guild.id;
+        if (msg.content.trim().toLowerCase() == _CMD_JOIN) {
+            if (!msg.member.voice.channelID) {
+                msg.reply('Error: please join a voice channel first.')
+            } else {
+                if (!guildMap.has(mapKey))
+                    await connect(msg, mapKey)
+                else
+                    msg.reply('Already connected')
+            }
+        } else if (msg.content.trim().toLowerCase() == _CMD_LEAVE) {
+            if (guildMap.has(mapKey)) {
+                let val = guildMap.get(mapKey);
+                if (val.voice_Channel) val.voice_Channel.leave()
+                if (val.voice_Connection) val.voice_Connection.disconnect()
+                guildMap.delete(mapKey)
+                msg.reply("Disconnected.")
+            } else {
+                msg.reply("Cannot leave because not connected.")
+            }
+        }
+    } catch (e) {
+        console.log('discordClient message: ' + e)
+        msg.reply('Error#180: Something went wrong, try again or contact the developers if this keeps happening.');
+    }
+})
+
+function getHelpString() {
+    let out = '**COMMANDS:**\n'
+        out += '```'
+        out += PREFIX + 'join\n';
+        out += PREFIX + 'leave\n';
+        out += '```'
+    return out;
+}
+
+const SILENCE_FRAME = Buffer.from([0xF8, 0xFF, 0xFE]);
+
+class Silence extends Readable {
+  _read() {
+    this.push(SILENCE_FRAME);
+    this.destroy();
+  }
+}
+
+async function connect(msg, mapKey) {
+    try {
+        let voice_Channel = await discordClient.channels.fetch(msg.member.voice.channelID);
+        if (!voice_Channel) return msg.reply("Error: The voice channel does not exist!");
+        let text_Channel = await discordClient.channels.fetch(msg.channel.id);
+        if (!text_Channel) return msg.reply("Error: The text channel does not exist!");
+        let voice_Connection = await voice_Channel.join();
+        voice_Connection.play(new Silence(), { type: 'opus' });
+        guildMap.set(mapKey, {
+            'text_Channel': text_Channel,
+            'voice_Channel': voice_Channel,
+            'voice_Connection': voice_Connection,
+            'selected_lang': 'ru',
+            'debug': false,
+        });
+        speak_impl(voice_Connection, mapKey)
+        voice_Connection.on('disconnect', async(e) => {
+            if (e) console.log(e);
+            guildMap.delete(mapKey);
+        })
+        msg.reply('connected!')
+    } catch (e) {
+        console.log('connect: ' + e)
+        msg.reply('Error: unable to join your voice channel.');
+        throw e;
+    }
+}
+
+const vosk = require('vosk');
+let recs = {}
+if (SPEECH_METHOD === 'vosk') {
+  vosk.setLogLevel(-1);
+
+  recs = {
+	'ru': new vosk.Recognizer({model: new vosk.Model('vosk_models/ru'), sampleRate: 48000})
+  }
+}
+
+
+function speak_impl(voice_Connection, mapKey) {
+    voice_Connection.on('speaking', async (user, speaking) => {
+        if (speaking.bitfield == 0 || user.bot) {
+            return
+        }
+        console.log(`I'm listening to ${user.username}`)
+        const audioStream = voice_Connection.receiver.createStream(user, { mode: 'pcm' })
+        audioStream.on('error',  (e) => { 
+            console.log('audioStream: ' + e)
+        });
+        let buffer = [];
+        audioStream.on('data', (data) => {
+            buffer.push(data)
+        })
+        audioStream.on('end', async () => {
+            buffer = Buffer.concat(buffer)
+            const duration = buffer.length / 48000 / 4;
+            console.log("duration: " + duration)
+
+            try {
+                let new_buffer = await convert_audio(buffer)
+                let out = await transcribe(new_buffer, mapKey);
+                if (out != null)
+				{	
+					if(out != "")
+					{
+						let val = guildMap.get(mapKey);
+						val.text_Channel.send(user.username + ': ' + out);
+						let user_event = { discord_id: user.id, discord_username: user.username, event_type: "speech", event_content: out};
+
+					}
+				}
+            } catch (e) {
+                console.log('tmpraw rename: ' + e)
+            }
+
+
+        })
+    })
+}
+
+function process_commands_query(txt, mapKey, user) {
+    if (txt && txt.length) {
+        let val = guildMap.get(mapKey);
+        val.text_Channel.send(user.username + ': ' + txt[txt.length-1])
+    }
+}
+
+
+//////////////////////////////////////////
+//////////////// SPEECH //////////////////
+//////////////////////////////////////////
+async function transcribe(buffer, mapKey) {
+	
+	let val = guildMap.get(mapKey);
+	recs[val.selected_lang].acceptWaveform(buffer);
+	let ret = recs[val.selected_lang].result().text;
+	console.log('vosk:', ret)
+	return ret;
+	
+}
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+//////////////////////////////////////////
+
